4baa9e677d416ba35f1d8fc191f3f12d348aa9e3
kimmascarenhas <39431019+kimmascarenhas@users.noreply.github.com>
2020-06-05 11:40:57 +0400

    KM-Added page 2 HTML , modified header and nav for page 1 HTML

---
+++ JS/project.js
@@ -0,0 +1,10 @@
+
+
+var answer = prompt("Hi there! Are you visiting us from the EU region?")
+if(answer.toLowerCase()=='yes') {
+    alert  ("Sorry! To continue browsing , you must accept cookies in the banner below so we don't get into trouble!");
+}
+else {
+    alert ( "Enjoy our site!");
+}
+
