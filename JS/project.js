49b734fbd0187d2b9a4780e747f94c7e5f771201
kimmascarenhas <39431019+kimmascarenhas@users.noreply.github.com>
2020-06-09 21:32:21 +0400

    Update project.js

--- JS/project.js
+++ JS/project.js
@@ -1,10 +1,12 @@
 
-
-var answer = prompt("Hi there! Are you visiting us from the EU region?")
-if(answer.toLowerCase()=='yes') {
-    alert  ("Sorry! To continue browsing , you must accept cookies in the banner below so we don't get into trouble!");
+if(localStorage.getItem('hasSeenPrompt') != 'true')
+{
+    var answer = prompt("Hi there! Are you visiting us from the EU region?")
+    if(answer.toLowerCase()=='yes' ) { 
+        alert  ("Sorry! To continue browsing , you must accept cookies in the banner below so we don't get into trouble!");
+    }
+    else {
+        alert ( "Enjoy our site!");
+        localStorage.hasSeenPrompt = true
+    }
 }
-else {
-    alert ( "Enjoy our site!");
-}
-
